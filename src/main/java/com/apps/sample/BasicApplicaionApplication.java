a526e8f8ffdce9bf995a7b93544b66b46013c287
Farhan464 <farhan.onmobile@gmail.com>
2019-09-20 14:27:22 +0400

    InitialCommit

---
+++ src/main/java/com/apps/sample/BasicApplicaionApplication.java
@@ -0,0 +1,13 @@
+package com.apps.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApplicaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApplicaionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apps/sample/BasicApplicaionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apps.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicApplicaionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
